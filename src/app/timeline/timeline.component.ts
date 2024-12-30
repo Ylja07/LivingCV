97d8e1ad35db15862423906693021b4da4b1ddac
YljavS <ylja.van.son@alliander.com>
2024-12-30 13:39:55 +0000

    dots added

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-timeline',
@@ -6,8 +6,15 @@ import { Component } from '@angular/core';
   templateUrl: './timeline.component.html',
   styleUrl: './timeline.component.scss'
 })
-export class TimelineComponent {
+export class TimelineComponent implements OnInit {
+  ngOnInit() {
+  }
+
 
   MAX_ITEMS_PER_PAGE: number = 3;
+
+  onDotClick(month: string): void {
+    console.log(`Clicked on: ${month}`);
+  }
 
 }
